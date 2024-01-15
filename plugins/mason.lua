0dab617929e425248d87d36f2f0f1472f1361326
acwwt <1420204102@qq.com>
2024-01-15 20:55:21 +0800

    main

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,36 +1,68 @@
+-- :e 重新加载语言分析服务
+-- :LSPInstall lua_ls
 -- customize mason plugins
+local getEvent = function()
+  if vim.g.lsp_type == "coc" then
+    return "User AstroFile"
+  else
+    return "LspAttach"
+  end
+end
+
 return {
+  {
+    -- This is needed for pylint to work in a virtualenv. See https://github.com/williamboman/mason.nvim/issues/668#issuecomment-1320859097
+    "williamboman/mason.nvim",
+  },
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
-      })
-    end,
   },
+  -- :NullLSInstall stylua
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
+  },
+  -- :DapInstall python
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+  {
+    "Weissle/persistent-breakpoints.nvim",
+    event = "BufReadPost",
     opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+      return require("astronvim.utils").extend_tbl(opts, {
+        load_breakpoints_event = { "BufReadPost" },
       })
     end,
   },
   {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
-      })
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup {
+        noice = true,
+        hint_prefix = " ",
+      }
     end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter" },
+    event = getEvent(),
+    opts = {
+      commented = true,
+      enabled = true, -- enable this plugin (the default)
+      enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+    },
+  },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    event = "LspAttach",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function() require("refactoring").setup {} end,
   },
 }
