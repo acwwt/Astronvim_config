0754820c50ff59495c880addb2401c348d242ea2
acwwt <1420204102@qq.com>
2024-01-20 19:21:00 +0800

    master

--- mappings.lua
+++ mappings.lua
@@ -29,8 +29,7 @@ return {
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
+    ["<leader>b"] = { name = "Buffers" },    -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   t = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,12 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+    -- lazydocker.nvim
+  {
+    "mgierada/lazydocker.nvim",
+    dependencies = { "akinsho/toggleterm.nvim" },
+    config = function() require("lazydocker").setup {} end,
+    event = "VeryLazy", -- or any other event you might want to use.
+  },
+
 }
